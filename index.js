8e0e43ae2263f0b5017e210e5e1645bda116063a
Ryan Schmukler <rschmukler@gmail.com>
2013-09-27 16:43:49 -0400

    Bug fix for duplicate listeners

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var ChangeSaved = module.exports = function(Model) {
 
   Model.on('saving', function(instance) {
     var dirty = instance.dirty;
-    instance.on('save', function() {
+    instance.once('save', function() {
       for(var key in dirty) {
         instance.emit('changeSaved:'+key, instance[key]());
         instance.model.emit('changeSaved:'+key, instance, instance[key]());
